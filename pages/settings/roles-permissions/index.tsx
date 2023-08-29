2497fe3ccba3df8007feb52f515346ffea22050f
iamwavefly <iamwavefly@gmail.com>
2023-08-29 19:57:19 +0100

    first commit

--- pages/settings/roles-permissions/index.tsx
+++ pages/settings/roles-permissions/index.tsx
@@ -5,7 +5,6 @@ import baseUrl from "@/middleware/baseUrl";
 import {
   Box,
   Button,
-  Checkbox,
   FormControl,
   FormControlLabel,
   Radio,
@@ -18,16 +17,19 @@ import {
 import React, { useEffect, useState } from "react";
 import BoxPlusIcon from "remixicon-react/AddBoxLineIcon";
 import PenIcon from "remixicon-react/EditLineIcon";
-import CheckboxIcon from "remixicon-react/CheckboxLineIcon";
+import Checkbox from "@/components/elements/Checkbox";
 import { useDispatch } from "react-redux";
 import NewSubsidiary from "@/components/form/newSubsidiary";
 import { setDrawalState } from "@/store/appSlice";
 import NewRole from "@/components/form/newRole";
 import Tabs from "@/components/Tabs";
+// icons
+import UserIcon from "@/public/icons/user-line.svg";
 
 interface Props {
   id: number;
   name: string;
+  tab: string;
   user_count: number;
 }
 
@@ -36,7 +38,9 @@ const Index = () => {
   const [permissions, setPermissions] = useState([]);
   const [users, setUsers] = useState([]);
   const [userPermission, setUserPermission] = useState([]);
-  const [roles, setRoles] = useState([]);
+  const [roles, setRoles] = useState<Props[]>([]);
+  const [currentTab, setCurrentTab] = useState(0);
+  const [selectedRole, setSelectedRole] = useState<Props>({} as Props);
 
   const dispatch = useDispatch();
   const { loading, data, error, handleSubmit } = useFetch(
@@ -45,7 +49,7 @@ const Index = () => {
   );
   // fetch permission based on role
   const permApi = useFetch(
-    `${baseUrl}/dashboard/role/details?roleid=${activeRole}`,
+    `${baseUrl}/dashboard/role/details?roleid=${currentTab}`,
     "get"
   );
   // permissions
@@ -57,20 +61,28 @@ const Index = () => {
   const updatePermission = useFetch(`${baseUrl}/dashboard/role/edit`);
 
   useEffect(() => {
+    setCurrentTab(roles?.[0]?.id);
+  }, [roles]);
+
+  useEffect(() => {
+    const newRole = roles?.find((role) => role.id === currentTab);
+    setSelectedRole(newRole as Props);
+    console.log({ newRole });
+  }, [roles, currentTab]);
+
+  useEffect(() => {
     handleSubmit();
   }, []);
 
   // fetch roles
   useEffect(() => {
-    const newRoles = data?.data?.map(
-      ({ id, name }: { id: number; name: string }) => {
-        return {
-          id,
-          tab: name,
-        };
-      }
-    );
-    console.log({ newRoles });
+    const newRoles = data?.data?.map(({ id, name, user_count }: Props) => {
+      return {
+        id,
+        tab: name,
+        count: user_count ?? 0,
+      };
+    });
     setRoles(newRoles);
   }, [data?.data]);
 
@@ -79,8 +91,8 @@ const Index = () => {
   }, []);
 
   useEffect(() => {
-    activeRole && permApi?.handleSubmit();
-  }, [activeRole]);
+    currentTab && permApi?.handleSubmit();
+  }, [currentTab]);
 
   useEffect(() => {
     setUserPermission(permApi?.data?.data?.permission);
@@ -158,6 +170,7 @@ const Index = () => {
             Roles & Permission
           </Typography>
           <Button variant="containedMedium" sx={{ height: "40px" }}>
+            <UserIcon />
             Create custom role
           </Button>
         </Stack>
@@ -173,128 +186,86 @@ const Index = () => {
             onChange={handleRoleChange}
             aria-label="text alignment"
           >
-            {/* {roles?.map(({ id, name, user_count }: Props) => (
-              <ToggleButton value={id} key={id}>
-                {name} ({user_count})
-              </ToggleButton>
-            ))} */}
-            <Tabs tabs={roles} />
+            <Tabs
+              tabs={roles as any}
+              updateTab={setCurrentTab}
+              currentTab={currentTab}
+            />
           </ToggleButtonGroup>
-          <Button
-            sx={{ height: "40px", fontSize: "12px", fontWeight: 500 }}
-            variant="outlined"
-            onClick={openDrawal}
-          >
-            <BoxPlusIcon size={18} />
-            Create custom role
-          </Button>
         </Stack>
-        <Typography mt="20px" fontSize="12px" color="rgba(38, 43, 64, 0.8)">
-          Admins with this role:{" "}
-          {users?.length
-            ? users?.map(({ id, name }) => name).join(", ")
-            : "N/A"}
-        </Typography>
-        <Box
-          padding="25px 34px"
-          width="100%"
-          height="auto"
-          maxWidth="788px"
-          minHeight="432px"
-          bgcolor="#fff"
-          mt="35px"
-        >
-          <Typography fontWeight={600} fontSize="14px" color="#262B40">
-            Permissions
-          </Typography>
-          <Stack mt="22px" spacing="20px">
-            {permissions?.map((perm: any) => {
-              const newPermission = userPermission?.find(
-                (fin: any) => fin.id === perm.id
-              ) as any;
-              if (newPermission) {
-                return (
-                  <Stack
-                    direction="row"
-                    justifyContent="space-between"
-                    key={newPermission?.id}
-                  >
-                    <Typography
-                      fontSize="14px"
-                      fontWeight={500}
-                      color="rgba(38, 43, 64, 0.8)"
-                    >
-                      {newPermission?.permission}
-                    </Typography>
-                    <Checkbox
-                      value={newPermission?.id}
-                      defaultChecked
-                      onChange={handleCheck}
-                    />
-                  </Stack>
-                );
-              } else {
-                return (
-                  <Stack
-                    direction="row"
-                    justifyContent="space-between"
-                    key={perm?.name}
-                  >
-                    <Typography
-                      fontSize="14px"
-                      fontWeight={500}
-                      color="rgba(38, 43, 64, 0.8)"
-                    >
-                      {perm?.name}
-                    </Typography>
-                    <Checkbox onChange={handleCheck} value={perm?.id} />
-                  </Stack>
-                );
-              }
-            })}
-            {/* {permissions?.length ? (
-              permissions?.map(
-                ({ id, permission }: { id: number; permission: string }) => (
-                  <Stack
-                    direction="row"
-                    justifyContent="space-between"
-                    key={id}
-                  >
-                    <Typography
-                      fontSize="14px"
-                      fontWeight={500}
-                      color="rgba(38, 43, 64, 0.8)"
-                    >
-                      {permission}
-                    </Typography>
-                    <CheckboxIcon color="#2E3192" size={20} />
-                  </Stack>
-                )
-              )
-            ) : (
-              <Typography
-                fontSize="14px"
-                fontWeight={500}
-                color="rgba(38, 43, 64, 0.8)"
-              >
-                No result found
-              </Typography>
-            )} */}
+        <Stack direction="row" spacing="88px" mt="46px">
+          <Stack spacing="12px" width="342px">
+            <Typography fontSize="18px" fontWeight={600} lineHeight="26px">
+              {selectedRole?.tab}
+            </Typography>
+            <Typography
+              fontSize="14px"
+              lineHeight="24px"
+              letterSpacing="0.14px"
+            >
+              Admin with this role:
+            </Typography>
           </Stack>
-        </Box>
-        {/* <Button
-          sx={{
-            height: "40px",
-            width: "max-content",
-            fontSize: "12px",
-            fontWeight: 500,
-            mt: "27px",
-          }}
-          variant="outlined"
-        >
-          <PenIcon size={18} />
-          Edit role
-        </Button> */}
+          <Box
+            padding="25px 34px"
+            width="100%"
+            height="auto"
+            maxWidth="788px"
+            minHeight="432px"
+            bgcolor="#fff"
+            mt="35px"
+          >
+            <Typography fontWeight={600} fontSize="14px" color="#262B40">
+              Permissions
+            </Typography>
+            <Stack mt="22px" spacing="20px">
+              {permissions?.map((perm: any) => {
+                const newPermission = userPermission?.find(
+                  (fin: any) => fin.id === perm.id
+                ) as any;
+                if (newPermission) {
+                  return (
+                    <Stack
+                      direction="row"
+                      justifyContent="space-between"
+                      key={newPermission?.id}
+                    >
+                      <Typography
+                        fontSize="14px"
+                        fontWeight={500}
+                        color="rgba(38, 43, 64, 0.8)"
+                      >
+                        {newPermission?.permission}
+                      </Typography>
+                      <Checkbox
+                        value={newPermission?.id}
+                        defaultChecked
+                        onChange={handleCheck}
+                      />
+                    </Stack>
+                  );
+                } else {
+                  return (
+                    <Stack
+                      direction="row"
+                      justifyContent="space-between"
+                      key={perm?.name}
+                    >
+                      <Typography
+                        fontSize="14px"
+                        fontWeight={500}
+                        color="rgba(38, 43, 64, 0.8)"
+                      >
+                        {perm?.name}
+                      </Typography>
+                      <Checkbox onChange={handleCheck} value={perm?.id} />
+                    </Stack>
+                  );
+                }
+              })}
+            </Stack>
+          </Box>
+        </Stack>
       </Stack>
     </Dashboard>
   );
