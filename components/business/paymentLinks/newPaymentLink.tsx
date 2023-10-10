6992d960d652253d5b9aa096fa61717502b46cc4
iamwavefly <iamwavefly@gmail.com>
2023-10-10 15:42:03 +0100

    reset new payment link form

--- components/business/paymentLinks/newPaymentLink.tsx
+++ components/business/paymentLinks/newPaymentLink.tsx
@@ -9,13 +9,12 @@ import React, { useEffect } from "react";
 import { toast } from "react-hot-toast";
 import { useDispatch } from "react-redux";
 
-export default function NewPaymentLink({ reload, details }: any) {
+export default function NewPaymentLink({ reload, details, close }: any) {
   const { loading, data, error, handleSubmit } = useFetch(
     `${baseUrl}/dashboard/payment/link/${details ? "edit" : "new"}`
   );
 
   const dispatch = useDispatch();
-  const close = () => dispatch(setDrawalState({ active: false }));
 
   const paymentTypes = useFetch(
     `${baseUrl}/dashboard/payment/link/types`,
@@ -31,14 +30,6 @@ export default function NewPaymentLink({ reload, details }: any) {
   useEffect(() => {
     paymentTypes?.handleSubmit();
   }, []);
-
-  useEffect(() => {
-    const { status, message } = data;
-    if (status === "Success") {
-      reload();
-      close();
-    }
-  }, [data]);
   // form controller
   const formik = useFormik({
     initialValues: {
@@ -70,6 +61,15 @@ export default function NewPaymentLink({ reload, details }: any) {
       handleSubmit(payload);
     },
   });
+
+  useEffect(() => {
+    const { status } = data;
+    if (status === "Success") {
+      reload();
+      close();
+      formik.resetForm();
+    }
+  }, [data]);
 
   useEffect(() => {
     if (details) {
