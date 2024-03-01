aba2c486b769a9c9d328280ea03a0becd57de099
iamwavefly <iamwavefly@gmail.com>
2024-03-01 04:21:33 +0100

    code clear

--- components/skeletons/post.tsx
+++ components/skeletons/post.tsx
@@ -1,11 +1,17 @@
 import { Skeleton, Stack } from "@mui/material";
 import React from "react";
+import Styles from "@/styles/product.module.scss";
 
 export default function PostSkeleton() {
   return (
-    <Stack>
+    <Stack
+      className={Styles.container}
+      border="1px solid rgba(0,0,0,0.03) !important"
+      pt="44px"
+      alignItems="center"
+    >
       <Skeleton
-        variant="rectangular"
+        variant="rounded"
         width={160}
         height={200}
         sx={{ mx: "auto" }}
@@ -16,6 +22,22 @@ export default function PostSkeleton() {
         height={26}
         sx={{ mt: "18px" }}
       />
+      <Skeleton
+        variant="rectangular"
+        width={205}
+        height={40}
+        sx={{ mt: "13px" }}
+      />
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        mt="36px"
+        width="100%"
+      >
+        <Skeleton variant="rectangular" width={90} height={26} />
+        <Skeleton variant="rounded" width={126} height={37} />
+      </Stack>
     </Stack>
   );
 }
