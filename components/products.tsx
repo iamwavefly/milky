40ce6b803f2e39af26e6c855d7543b18e94ec989
iamwavefly <iamwavefly@gmail.com>
2024-03-01 04:13:04 +0100

    code clear

--- components/products.tsx
+++ components/products.tsx
@@ -1,25 +1,31 @@
 import { ProductsTypes } from "@/types";
 import { _products } from "@/mocks";
 import { Grid } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Product from "./product";
 import { useSelector } from "react-redux";
 import { AppState } from "@/store/store";
 import PostSkeleton from "./skeletons/post";
 
 export default function Products({ limit, ...others }: ProductsTypes) {
+  const [loading, setLoading] = useState(true);
   const searchQuery = useSelector((state: AppState) => state.cart.searchQuery);
 
   const filteredProducts = _products.filter((product) =>
     product.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <Grid container {...others} spacing="18px">
       {filteredProducts?.slice(0, limit)?.map((product) => (
         <Grid item key={product?.id} md={3}>
-          {/* <Product {...product} /> */}
-          <PostSkeleton />
+          {loading ? <PostSkeleton /> : <Product {...product} />}
         </Grid>
       ))}
     </Grid>
